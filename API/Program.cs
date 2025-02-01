a5f9d8765f1b1dc7d0adc07b21601d9f3f20e157
danclarke95 <dan.clarke1995@gmail.com>
2025-02-01 14:27:47 +0000

    End of Section 5

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddIdentityService(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+
 
 app.UseAuthentication();
 app.UseAuthorization();
